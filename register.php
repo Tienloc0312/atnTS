485df48cf04fe87fce060c4bd086697fce70a6df
Tienloc0312 <105624434+Tienloc0312@users.noreply.github.com>
2022-07-29 11:17:33 +0700

    yu

--- register.php
+++ register.php
@@ -62,7 +62,7 @@
 			</table>
 		</div>
 	   <?php 
-	      $connect =mysqli_connect('3.132.234.157','tienloc','tienloc123','tsasm');
+	      $connect =mysqli_connect('localhost','root','','mydb');
 	      if($connect){
 	      	echo "kết nối thành công";
 	      }
